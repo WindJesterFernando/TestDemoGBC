c2f58ce9dfa1c5a368719dc4294ea73344b4c2a4
Fernando Restituto <linksgreenhat@gmail.com>
2022-10-07 09:25:05 -0400

    super awesome changes

--- LearnC++/LearnC++/main.cpp
+++ LearnC++/LearnC++/main.cpp
@@ -9,6 +9,7 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!, in the year 2000\n";
+    std::cout << "Super Awesome Change\n";
     return 0;
 }
